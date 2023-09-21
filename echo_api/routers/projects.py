728b5d3c6842b2ce2f1b4f5d5ba6af4c8834df29
RasterMk2 <70229194+RasterMk2@users.noreply.github.com>
2023-09-21 18:13:21 -0400

    Added get_project dependency function.

--- echo_api/routers/projects.py
+++ echo_api/routers/projects.py
@@ -1,11 +1,27 @@
-from typing import Sequence
+from typing import Annotated, Sequence
 
-from fastapi import APIRouter, HTTPException, UploadFile
+from fastapi import APIRouter, Depends, HTTPException, UploadFile
 from fastapi.responses import FileResponse, Response
 
 from echo_api import auth, crud, database, schemas
 
 projects_router: APIRouter = APIRouter()
+
+
+async def get_project_dep(
+    project_id: int, db: database.db_depends, current_user: auth.user_depends
+) -> database.Project:
+    project = await crud.get_project(db, project_id)
+    if project is None:
+        raise HTTPException(status_code=404, detail="Project does not exist.")
+    if current_user.id != project.owner_id:
+        raise HTTPException(
+            status_code=403, detail="You are not authorized to view this project."
+        )
+    return project
+
+
+project_depends = Annotated[database.Project, Depends(get_project_dep)]
 
 
 @projects_router.post("/", response_model=schemas.Project, tags=["projects", "create"])
@@ -32,50 +48,19 @@ async def get_projects(
 @projects_router.get(
     "/{project_id}", response_model=schemas.Project, tags=["projects", "get"]
 )
-async def get_project(
-    project_id: int, db: database.db_depends, current_user: auth.user_depends
-) -> database.Project:
-    project = await crud.get_project(db, project_id)
-    if project is None:
-        raise HTTPException(status_code=404, detail="Project does not exist.")
-    if current_user.id != project.owner_id:
-        raise HTTPException(
-            status_code=403, detail="You are not authorized to view this project."
-        )
+async def get_project(project: project_depends) -> database.Project:
     return project
 
 
 @projects_router.get(
     "/file/{project_id}", response_class=FileResponse, tags=["projects", "get"]
 )
-async def get_project_file(
-    project_id: int, db: database.db_depends, current_user: auth.user_depends
-) -> FileResponse:
-    project = await crud.get_project(db, project_id)
-    if project is None:
-        raise HTTPException(status_code=404, detail="Project does not exist.")
-    if current_user.id != project.owner_id:
-        raise HTTPException(
-            status_code=403, detail="You are not authorized to view this project."
-        )
-    return await crud.read_project_file(project_id)
+async def get_project_file(project: project_depends) -> FileResponse:
+    return await crud.read_project_file(project.id)
 
 
 @projects_router.put("/file/{project_id}", tags=["projects", "get"], status_code=204)
-async def write_project_file(
-    project_id: int,
-    file: UploadFile,
-    db: database.db_depends,
-    current_user: auth.user_depends,
-) -> Response:
-    project = await crud.get_project(db, project_id)
-    if project is None:
-        raise HTTPException(status_code=404, detail="Project does not exist.")
-    if current_user.id != project.owner_id:
-        raise HTTPException(
-            status_code=403, detail="You are not authorized to view this project."
-        )
-
-    await crud.upload_project_file(project_id, file)
+async def write_project_file(file: UploadFile, project: project_depends) -> Response:
+    await crud.upload_project_file(project.id, file)
 
     return Response(status_code=204)
